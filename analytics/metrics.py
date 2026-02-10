345bfb501b7368e424f637df0d49bb3149a220bf
Ashish chaudhary <36606232+ashishc628@users.noreply.github.com>
2026-02-10 14:03:32 +0530

    Delete analytics directory

--- analytics/metrics.py
+++
@@ -1,6 +0,0 @@
-def aggregate(df):
-    return {
-        "avg_sleep": df["sleep_duration_hours"].mean(),
-        "avg_stress": df["stress_level"].mean(),
-        "completion_rate": df["completed_session"].mean()
-    }
