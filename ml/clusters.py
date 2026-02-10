980d420a953c0a150050e0e054796f1ff3b4aae4
Ashish chaudhary <36606232+ashishc628@users.noreply.github.com>
2026-02-10 14:04:15 +0530

    Delete ml directory

--- ml/clusters.py
+++
@@ -1,7 +0,0 @@
-def map_cluster_to_profile(cluster_id):
-    return {
-        0: "low_energy",
-        1: "high_stress",
-        2: "balanced",
-        3: "high_focus"
-    }.get(cluster_id, "balanced")
--- ml/rules.py
+++
@@ -1,8 +0,0 @@
-def apply_safety_rules(user, duration):
-    if user["sleep_duration_hours"] < 6:
-        return min(duration, 5)
-
-    if user["stress_level"] >= 4:
-        return min(duration, 4)
-
-    return max(duration, 2)
