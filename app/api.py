79d21cb49053322d33106da9401a112070578269
Ashish chaudhary <36606232+ashishc628@users.noreply.github.com>
2026-02-10 14:03:41 +0530

    Delete app directory

--- app/api.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from app.schemas import UserInput
-from ml.model import recommend
-
-router = APIRouter()
-
-@router.post("/recommend")
-def get_recommendation(data: UserInput):
-    return recommend(data.dict())
